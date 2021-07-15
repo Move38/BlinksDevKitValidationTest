350ab5398731171c1e79ec4a03f97eb5866c907d
Jonathan Bobrow <jbobrow@gmail.com>
2021-07-15 06:41:52 -0400

    Update the Blinks validation sketch to include forced cold sleep

--- BlinksDevKitValidationTest.ino
+++ BlinksDevKitValidationTest.ino
@@ -1,44 +1,32 @@
 /*
    Blinks Dev-Kit
-
    Validation Test for Dev Kit Blinks
-
-   1) Validate RGB LEDs
-   2) Validate Button
-   3) Validate IR Comms
-
-   Cycle RED, GREEN, BLUEto verify all 6 LEDs light
-   - look for equal brightness
-   - look for discreet light, i.e. no two colors should be on at the same time
-
-   Press button to switch modes
-   When button is pressed down, the lights should glow WHITE (RGB together)
-
-   Communication mode is dark by default, 
-   When neighboring Blink is in Communication mode, show CYAN and MAGENTA data
-
-   Hold button until YELLOW to enter send mode
-   Send Mode is for surrounding Blinks to be used to test a single Blink
-   Shows YELLOW on sides without Blinks attached
-   Show CYAN and MAGENTA for Blink attached
-
-   A little bit of serial printing just in case we want to also look at what is executing via serial monitor
-
+   4 modes for testing
+   1. Verify RGB on all 6 faces together
+   2. Verify RGB on each of the 6 faces individually
+   3. Verify communication w/ awake neighbors
+   4. Verify cold sleep (<0.2µA)
    by Jonathan Bobrow
-   05.10.2018
+   02.25.2020
 */
 
 #include "Serial.h"
 
+uint8_t sterileFlag=1; // Make this game sterile.
 
-#define COLOR_DURATION_MS 500
+#define BLINKBIOS_SLEEP_NOW_VECTOR boot_vector12
+// Calling BLINKBIOS_SLEEP_NOW_VECTOR() will immedeately put the blink into hardware sleep
+// It can only wake up from a button press.
+extern "C" void BLINKBIOS_SLEEP_NOW_VECTOR()  __attribute__((used)) __attribute__((noinline));
+
+#define COLOR_DURATION_MS 1200
 #define MESSAGE_DURATION_MS 250
-#define LONG_MESSAGE_DURATION_MS 500
 
 enum Mode {
-  DISPLAY,
-  COMMS,
-  SENDMODE
+  DISPLAY_ALL,
+  DISPLAY_PIXEL,
+  DISPLAY_COMMS,
+  GO_TO_SLEEP
 };
 
 Color colors[] = {RED, GREEN, BLUE};
@@ -51,6 +39,8 @@ Timer colorSwitch;
 Timer messageSwitch;
 Timer longMessageSwitch;
 
+boolean goto_sleep_flag = false;
+
 ServicePortSerial Serial;
 
 void setup() {
@@ -61,73 +51,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if ( mode == GO_TO_SLEEP ) {
+
+    BLINKBIOS_SLEEP_NOW_VECTOR();
+    mode = DISPLAY_ALL;
+
+  }
+
   if ( buttonPressed() ) {
 
-    Serial.println("Button pressed, serial printing confirmed :)");
+    mode++;  // switch modes
 
-    // switch modes
-    mode++;
+    // No need to test for wrap here since we jump back to 1st mode (all) after last mode (sleep)
 
-    if (mode >= 2) {
-      mode = 0;
-    }
+    Serial.print("Button pressed, updated mode to ");
+    Serial.print(mode);
+    Serial.println(" and serial printing confirmed :)");
   }
 
-  if ( buttonLongPressed() ) {
-
-    Serial.println("Button long pressed. Entering only send mode");
-
-    // switch modes
-    mode = SENDMODE;
-  }
-
-  if ( mode == DISPLAY ) {
-
-    // make sure the message on all sides is 0
-    setValueSentOnAllFaces(0);
-
-    // switch colors every COLOR_DURATION_MS
-    if ( colorSwitch.isExpired() ) {
-
-      colorSwitch.set( COLOR_DURATION_MS );
-
-      colorIndex = (colorIndex + 1) % COUNT_OF(colors);
-    }
-
-    // display the current color
-    setColor( colors[colorIndex] );
-  }
-
-  if ( mode == COMMS ) {
-
-    // send
-    if ( messageSwitch.isExpired() ) {
-
-      messageSwitch.set( MESSAGE_DURATION_MS );
-
-      message = (message % 2) + 1;
-
-    }
-
-    setValueSentOnAllFaces( message );
-
-    // reset display
-    setColor(OFF);
-
-    // show received value
-    FOREACH_FACE(f) {
-      if (!isValueReceivedOnFaceExpired(f)) {
-
-        byte faceValue = getLastValueReceivedOnFace(f);
-
-        if ( faceValue == 1 ) {
-          setFaceColor(f, MAGENTA);
-        }
-        else if ( faceValue == 2 ) {
-          setFaceColor(f, CYAN);
-        }
-      }
-    }
+  switch (mode) {
+    case DISPLAY_ALL:     displayAll();                   break;
+    case DISPLAY_PIXEL:   displayPixel();                 break;
+    case DISPLAY_COMMS:   displayComms();                 break;
+    case GO_TO_SLEEP:     displaySleep();                 break;
 
   }
 
@@ -136,36 +82,54 @@ void loop() {
     // glow white to show we are down
     setColor(WHITE);
   }
+}
 
-  
-  if ( mode == SENDMODE ) {
+void displayAll() {
 
-    // send
-    if ( longMessageSwitch.isExpired() ) {
+  // switch colors every COLOR_DURATION_MS
+  if ( colorSwitch.isExpired() ) {
 
-      longMessageSwitch.set( LONG_MESSAGE_DURATION_MS );
+    colorSwitch.set( COLOR_DURATION_MS );
 
-      message = (message % 2) + 1;
-
-    }
-
-    setValueSentOnAllFaces( message );
-
-    // reset display
-    setColor(YELLOW);
-
-    // show sending value
-    FOREACH_FACE(f) {
-      if (!isValueReceivedOnFaceExpired(f)) {
-
-        if ( message == 1 ) {
-          setFaceColor(f, MAGENTA);
-        }
-        else if ( message == 2 ) {
-          setFaceColor(f, CYAN);
-        }
-      }
-    }
-
+    colorIndex = (colorIndex + 1) % COUNT_OF(colors);
   }
+
+  // display the current color
+  setColor( colors[colorIndex] );
+}
+
+void displayPixel() {
+
+  // switch colors every COLOR_DURATION_MS
+  if ( colorSwitch.isExpired() ) {
+
+    colorSwitch.set( COLOR_DURATION_MS );
+
+    colorIndex = (colorIndex + 1) % COUNT_OF(colors);
+  }
+
+  // display the current color
+  byte index = (COLOR_DURATION_MS - colorSwitch.getRemaining()) / 200;
+  setColor(OFF);
+  setColorOnFace( colors[colorIndex], index);
+}
+
+void displayComms() {
+
+  // reset display
+  setColor(dim(WHITE, 64));
+
+  // show received value
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {
+      setFaceColor(f, WHITE);
+    }
+  }
+
+}
+
+void displaySleep() {
+
+  setColor(OFF);
+
 }
